d185667ccc537e8d16fecfc06467cf72694db7a6
Ippo03 <106438453+Ippo03@users.noreply.github.com>
2023-02-25 16:16:44 +0200

    Add files via uplaod

--- Semester 3/Assembly/Assignment 3/Postfix Calculator.s
+++ Semester 3/Assembly/Assignment 3/Postfix Calculator.s
@@ -1,208 +1,223 @@
-	#IPPOKRATIS PANTELIDIS p3210150
+#PANTELIDIS IPPOKRATIS p3210150
+#MITSAKIS NIKOS p3210122
+#PANAGIOTIS MOSCHOS p3210127	
+
+.text
+.globl main
+#main
+main:	
+	#result
+	li $t0,0 
+	#values in stack
+	li $t2,0							
+  			
+	#print mhnyma 1
+    li $v0,4							
+    la $a0, input_prompt
+    syscall
 	
-	.data
-num1:       .space 4			#prwtos ari8mos(4 bytes)
-num2:       .space 4  			#deyterosari8mos(4 bytes)
-res:        .space 4			#apotelesma(4 bytes)
-op:         .space 1			#telesths(1 byte)
-test1:		.asciiz "test"		#mynhmata
-prompt:     .asciiz "\nNumber:" #1
-test2:		.asciiz "test1"
-prompt1:    .asciiz "Operator:" #2	
-prompt2:    .asciiz "\nError: Invalid operator." #3
-prompt3:    .asciiz "Error: Divide by zero."	#4
-prompt4:    .asciiz "\nDo you want to continue with a new expression? (y/n)" #5
-prompt5:	.asciiz "\nThe result is:" #6
-ans:		.space 1			#telesths(1 byte)
-			
-		    .text
-			.globl main
+read_token: 
+	#read ch(char) 
+    li $v0, 12							
+    syscall
 	
-	main:	
-		
-	loop1:	la $a0,prompt		#Emfanise mynhma 1
-			li $v0,4
-			syscall
-			
-			li $v0,5			#read num1
-			syscall
-			
-			sw $v0,res			#apotelesma = num1
-			lw $t0,res
-			
-			la $a0,prompt1		#Emfanise mynhma 2
-			li $v0,4
-			syscall
-			
-			li $v0,12			#read operator
-			syscall
-			
-			sh $v0,op		
-			lb $t1,op
-			
-			li $t3,'+'			#anatheseis
-			li $t4,'-'
-			li $t5,'*'
-			li $t6,'/';
-			li $t7,'%'
-			li $t8,'='
-			
-	s0:		bne $t1,$t3,s1		#if op=='+' kane prosthesi else go to s1
-			la $a0,prompt 		#Emfanise mynhma 1
-			li $v0,4
-			syscall
-				
-			li $v0,5			#read num2
-			syscall
-			sw $v0,num2
-			lw $t2,num2
-			
-			add $t0,$t0,$t2
-			
-			la $a0,prompt1 		#Emfanise mynhma 2
-			li $v0,4
-			syscall
-			
-			li $v0,12			#read op
-			syscall
-			
-			sh $v0,op			
-			lb $t1,op
-			
-	s1:		bne $t1,$t4,s2		#if op=='-' kane afairesh else go to s2
-			la $a0,prompt		#Emfanise mynhma 1
-			li $v0,4
-			syscall
-				
-			li $v0,5			#read num2
-			syscall
-			sw $v0,num2
-			lw $t2,num2
-			
-			sub $t0,$t0,$t2
-			
-			la $a0,prompt1 		#Emfanise mynhma 2
-			li $v0,4
-			syscall
-			
-			li $v0,12			#read op
-			syscall
-			
-			sh $v0,op			
-			lb $t1,op
-			
-	s2:   	bne $t1,$t5,s3		#if op=='*' kane afairesh else go to s3
-			la $a0,prompt		#Emfanise mynhma 1
-			li $v0,4
-			syscall
-				
-			li $v0,5			#read num2
-			syscall
-			sw $v0,num2
-			lw $t2,num2
-			
-			mul $t0,$t0,$t2
-			
-			la $a0,prompt1		#Emfanise mynhma 2
-			li $v0,4
-			syscall
-			
-			li $v0,12			#read op
-			syscall
-			
-			sh $v0,op			
-			lb $t1,op
-			
-	s3:		bne $t1,$t6,s4		#if op=='/' kane diairesh else go to s4
-			la $a0,prompt		#Emfanise mynhma 1
-			li $v0,4
-			syscall
-			
-			li $v0,5			#read num2
-			syscall
-			sw $v0,num2
-			lw $t2,num2
-			
-			bne $t2,$zero,divi	#if num!=0 go to divi else Emfanise mynhma 4
-			la $a0,prompt3
-			li $v0,4
-			syscall
-			
-			j exit				#go to exit
-			
-			
-	s4:		bne $t1,$t7,s5		#if op=='%' kane modulo else go to s5
-			la $a0,prompt		#Emfanise mynhma 1
-			li $v0,4
-			syscall
-			
-			li $v0,5			#read num2
-			syscall
-			sw $v0,num2
-			lw $t2,num2
-			
-			bne $t2,$zero,modu	#if num!=0 go to modu else Emfanise mynhma 4
-			la $a0,prompt3
-			li $v0,4
-			syscall
-			
-			j exit	#go to exit
-			
-			
-	divi:
-			div $t0,$t0,$t2
-			
-			la $a0,prompt1 		#Emfanise mynhma 2
-			li $v0,4
-			syscall
-			
-			li $v0,12			#read op
-			syscall
-			
-			sh $v0,op			
-			lb $t1,op
-			
-			j s0
-	modu:	
-			rem $t0,$t0,$t2
-			la $a0,prompt1 		#Emfanise mynhma 2
-			li $v0,4
-			syscall
-			
-			li $v0,12			#read op
-			syscall
-			
-			sh $v0,op			
-			lb $t1,op		
-			
-			j s0
-			
-	s5:		beq $t1,$t8,s6		#if op=='=' emfanise mynhma 3 else go to s6
-			la $a0,prompt2	
-			li $v0,4
-			syscall
-		    j exit				#go to exit
+	#load ch in $t1
+	sh $v0,ch							
+	lb $t1,ch
 	
-	s6:		la $a0,prompt5		#emafanise mynhma 6
-			li $v0,4
-		    syscall
-										
-			move $a0,$t0		#Ektypwse to apotelesma
-			li $v0,1
-		    syscall
-			
-			la $a0,prompt4		#Emafanise mynhma synexeias 5
-			li $v0,4
-		    syscall
-			
-			li $v0,12			#read op
-			syscall
-			
-			sh $v0,ans			
-			lb $t9,ans
-			
-			beq $t9,'y',loop1 	#if ans=='y' go to loop1
-			j exit				#go to exit
-			
-	exit:	li $v0,10
-			syscall
+	#if(ch == ' ') read next ch
+	beq $t1,' ',read_token		
+	#number = 0
+	li $t5,0	
+
+#while ((ch >='0') && (ch<='9'))	
+digit:		
+	#if ch < ascii code of 0 go to label checkop
+	blt $t1,48,checkop	
+	#if ch > ascii code of 9 go to label checkop
+	bgt $t1,57,checkop	
+	#int value of ch
+	addi $t1, $t1, -48 	
+	#10*number
+	mul $t5,$t5,10	
+	#number = 10*number + (ch-48)
+	add $t5,$t5,$t1						
+	
+	#read next ch
+	li $v0,12							
+    syscall
+	
+	#load ch in $t1
+	sh $v0,ch							
+	lb $t1,ch
+	
+	#jump to the start of the while loop
+	j digit								
+
+#ch not ' ', not value,check if operator,true goto to operator	
+checkop:								
+	beq $t1,'+',operator
+	beq $t1,'-',operator
+	beq $t1,'*',operator
+	beq $t1,'/',operator
+	#if not +,-,*,/ goto label notequal
+	j not_equal							
+	
+operator:
+	#call pop function
+	jal pop		
+	#x2= pop()
+    move $t6, $v0	
+	#call pop function
+    jal pop	
+	#x1= pop()
+    move $t7, $v0
+	#calc arguments
+	#x2
+	move $a0, $t6 
+	#ch
+    move $a1, $t1
+	#x1
+    move $a2, $t7 
+	#call calc function
+    jal calc
+	#return value of calc->argument for push
+	move $a0,$v0
+	#call push function
+	jal push		
+	#go to label read_token
+	j read_token						
+
+not_equal: 
+	#if ch == '=' go to final
+	beq $t1,'=',final
+	#number->argument for push
+	move $a0,$t5
+	#call push function
+	jal push
+	#go to label read_token
+	j read_token						
+	
+final:
+	#if stack has one value print else goto label invalid
+	bne $t2,1,invalid
+	#print mynhma 5
+	li $v0, 4							
+    la $a0, output_msg
+    syscall
+	
+	#load and print value at the top of the stack
+    lw $a0,0($sp)						
+    li $v0,1							
+    syscall
+	
+	#goto exit
+	j exit 								
+
+#subprogramms
+#pop function
+pop:			
+	#check if stack is empty
+    beq $t2,0,invalid
+	#decrease values in stack by 1
+	sub $t2,$t2,1	
+	#load value at the top of the stack
+    lw $v0,0($sp)	
+	#increase stack pointer by 4
+	add $sp,$sp,4	
+	#return where called
+    jr $ra								
+
+#calc function
+calc:	
+	#check value of ch and perform calculation
+    beq $a1, '+', plus					
+    beq $a1, '-', minus
+    beq $a1, '*', times
+    beq $a1, '/', divide
+    j end
+
+plus:	
+	#x1 + x2
+    add $v0, $a2, $a0					
+    j end
+
+minus:
+	#x1 - x2
+    sub $v0, $a2, $a0					
+    j end
+
+times:
+	# x1 * x2
+    mul $v0, $a2, $a0					
+    j end
+
+divide:
+	#if x2 != 0 else goto label divide_by_zero
+    beq $a0, 0, divide_by_zero	
+	#x1 / x2
+    div $v0, $a2, $a0					
+    j end
+
+divide_by_zero:
+	#print mynhma 4
+    li $v0, 4							
+    la $a0, error_div
+    syscall
+	
+	#goto exit
+    j exit								
+
+end:
+	#return where called
+    jr $ra								
+	
+#push function
+push:		
+	#check if stack is full
+    beq $t2,20,full	
+	#increase values in stack by 1
+	add $t2,$t2,1	
+	#decrease stack pointer by 4
+    add $sp, $sp,-4			
+	#store value at the top of the stack
+	sw $a0,0($sp)
+	#return where called					
+    jr $ra								
+
+invalid:
+	#print mynhma 2
+    li $v0, 4							
+    la $a0, error_msg
+    syscall 
+	
+	#goto exit
+	j exit								
+	
+full:
+	#print mynhma 3
+    li $v0, 4							
+    la $a0, full_stack
+    syscall
+	
+	#goto exit
+	j exit								
+   
+exit: 
+	#exit program
+	li $v0,10							
+	syscall
+	
+.data
+#character(1 byte)
+ch:			  .space 1					
+#mhnyma 1		
+input_prompt: .asciiz "Postfix (input):"	
+#mhnyma 2	
+error_msg: .asciiz "\nInvalid Postfix"		
+#mhnyma 3	
+full_stack: .asciiz "\nThe stack is full"		
+#mhnyma 4
+error_div: .asciiz "\nDivide by zero"		
+#mhnyma 5	
+output_msg: .asciiz "\nPostfix Evaluation: "	
