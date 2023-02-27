5f010a5fab6a1b9fb4f6581513a023fa6d0e8406
Ippo03 <106438453+Ippo03@users.noreply.github.com>
2023-02-27 23:58:22 +0200

    Add files via upload

--- Semester 2/Java/hw2/App1.java
+++ Semester 2/Java/hw2/App1.java
@@ -8,7 +8,7 @@ class App1 {
     // Fill your code here (Factorial Method)
 	public int factorial(int n) {
 		int fact = 1;
-	    for(int i = 1; i <= n; i++) {
+		for(int i = 1; i <= n; i++) {
 			fact = fact*i;
 		}
 		return fact;
@@ -18,13 +18,13 @@ class App1 {
 	public static void main(String args[]) {
 		
 		// Fill your code here 
-     	int n = Integer.parseInt(args[0]);
+     		int n = Integer.parseInt(args[0]);
 		App1 ob = new App1();
 		if (n < 0 ){
 			System.out.println("Cant find factorial.");
 		} else {
 			System.out.println("Factorial = " + ob.factorial(n));
-			}
+		}
 	}
 }
 
